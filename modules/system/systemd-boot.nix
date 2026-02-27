519d0d504ba144dd5ec8f7b8f47b3e4c394687be
l41twz <fortesuplementosalimentares@gmail.com>
2026-02-27 16:32:22 -0300

    modules/system: add missing NIXOS-LEGO-MODULE header to systemd-boot so lego-tui parses it

--- modules/system/systemd-boot.nix
+++ modules/system/systemd-boot.nix
@@ -1,2 +1,6 @@
+# NIXOS-LEGO-MODULE: systemd-boot
+# PURPOSE: Habilita o Systemd-boot (UEFI) como bootloader padrao
+# CATEGORY: system
+# ---
 boot.loader.systemd-boot.enable = true;
 boot.loader.efi.canTouchEfiVariables = true;
